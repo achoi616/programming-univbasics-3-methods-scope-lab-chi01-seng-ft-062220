bb1933031e32aa74da4c67bf08a960fca18f824f
achoi616 <achoi616@gmail.com>
2020-06-15 19:43:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,17 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  puts status
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases 
+  
+
 end
